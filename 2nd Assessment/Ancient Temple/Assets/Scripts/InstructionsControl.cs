d5548d5c0eaacbd36bc6159c8278731326051b53
Ashley Cowell <ac215888@falmouth.ac.uk>
2019-04-18 15:25:06 +0100

    Programmed the instructions pages

---
+++ 2nd Assessment/Ancient Temple/Assets/Scripts/InstructionsControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InstructionsControl : MonoBehaviour {
+
+    public Canvas dangersCanvas;
+    public Canvas controlsCanvas;
+    public Canvas informationCanvas;
+
+	public void instructionsInfo()
+    {
+        informationCanvas.gameObject.SetActive(true);
+        controlsCanvas.gameObject.SetActive(false);
+        dangersCanvas.gameObject.SetActive(false);
+    }
+
+    public void instructionsDangers()
+    {
+        informationCanvas.gameObject.SetActive(false);
+        controlsCanvas.gameObject.SetActive(false);
+        dangersCanvas.gameObject.SetActive(true);
+    }
+
+    public void instructionsControls()
+    {
+        informationCanvas.gameObject.SetActive(false);
+        controlsCanvas.gameObject.SetActive(true);
+        dangersCanvas.gameObject.SetActive(false);
+    }
+}
